a0f5d1b292c38e3d6f172179c4a17a32f953390b
Federico <federicolucaspontoriero@gmail.com>
2022-07-15 15:23:32 -0300

    First program and cargo setup

---
+++ chapters/target/debug/deps/chapters-31678cfe65b58391.d
@@ -0,0 +1,5 @@
+/home/fede/Proyectos/rust-book/chapters/target/debug/deps/chapters-31678cfe65b58391.rmeta: src/main.rs
+
+/home/fede/Proyectos/rust-book/chapters/target/debug/deps/chapters-31678cfe65b58391.d: src/main.rs
+
+src/main.rs:
---
+++ chapters/target/debug/deps/chapters-b2718ffaacae4ff7.d
@@ -0,0 +1,7 @@
+/home/fede/Proyectos/rust-book/chapters/target/debug/deps/chapters-b2718ffaacae4ff7.rmeta: src/main.rs
+
+/home/fede/Proyectos/rust-book/chapters/target/debug/deps/chapters-b2718ffaacae4ff7.d: src/main.rs
+
+src/main.rs:
+
+# env-dep:CLIPPY_ARGS=
